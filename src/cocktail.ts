27adc4c9f04c5706ebd925539cf40267653ad235
pac <pac@MBP-de-Marina.lan>
2024-01-08 18:15:00 +0100

    fix syntax error

--- src/cocktail.ts
+++ src/cocktail.ts
@@ -1,26 +1,23 @@
 import fs from "fs";
-import { Cocktail } from "./types";
+import { Cocktail, CocktailApiResponse } from "./types";
 
 export function getData(url: string): Array<Cocktail> | null {
   try {
     const data = fs.readFileSync(url, "utf8");
-    return JSON.parse(data);
+    const json = JSON.parse(data);
+    return json.cocktails;
   } catch (error) {
     console.error("Erreur lors de la lecture du fichier JSON :", error);
-    // @ts-ignore
     return null;
   }
 }
 
 export async function callExternalAPI(): Promise<any | null> {
   try {
-    // Remplacez l'URL par l'URL de l'API externe que vous souhaitez appeler
-    const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-
-    // Utilisez axios pour effectuer la requête GET
-    const data = await fetch(apiUrl);
-
-    return data;
+    const apiUrl: string = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+    const response = await fetch(apiUrl);
+    const data: CocktailApiResponse = await response.json();
+    return data.drinks[0];
   } catch (error) {
     console.error("Erreur lors de l'appel à l'API externe :", error);
     return null;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // src/index.js
-import express, { Express, Request, Response, Router } from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { callExternalAPI, getData } from "./cocktail";
-import { Cocktail } from "./types";
+import { Cocktail, DrinkDetails } from "./types";
 
 dotenv.config();
 
@@ -21,23 +21,19 @@ app.get("/", function (req: Request, res: Response, next: Function) {
 });
 
 app.get("/commandeCocktail", async function (req: Request, res: Response) {
-  const cocktailStock: Array<Cocktail> | null = getData(dataPath);
-  const demandeClient = await callExternalAPI();
-
-  if (cocktailStock && demandeClient?.drinks && demandeClient.drinks.length > 0) {
-    const nomBoisson = demandeClient.drinks[0].strDrink;
-
-    const matchingCocktail = cocktailStock.find(
-      (cocktail) => cocktail.nom === nomBoisson
+  const cocktails: Array<Cocktail> | null = getData(dataPath);
+  const customerOrder: DrinkDetails | null = await callExternalAPI();
+  if (cocktails && customerOrder) {
+    const matchingCocktail = cocktails.find(
+      (cocktail) => cocktail.nom === customerOrder.strDrink
     );
     if (matchingCocktail) {
-      res.json("C'est un " + matchingCocktail + " qui part");
+      res.json("C'est un " + matchingCocktail.nom + " qui part");
     } else {
       res.json("J'ai plus de stock");
     }
   } else {
-    const codeErreur: number = 500;
-    res.status(codeErreur).send("Erreur lors de la lecture du fichier JSON");
+    res.status(500).send("Erreur lors de la lecture du fichier JSON");
   }
 });
 
